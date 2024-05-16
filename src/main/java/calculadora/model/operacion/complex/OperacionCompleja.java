817e706ab3a95c6a28f75d30827a68b29e202233
Santiago <santicastellanos06@gmail.com>
2024-05-16 13:52:50 -0500

    Operacion completa terminada

--- src/main/java/calculadora/model/operacion/complex/OperacionCompleja.java
+++ src/main/java/calculadora/model/operacion/complex/OperacionCompleja.java
@@ -4,23 +4,22 @@ public class OperacionCompleja {
     
     // Método para realizar la suma de dos números complejos
     public String suma(String n1, String n2) {
-        int x=0;
-        throw new UnsupportedOperationException("No implementada");
+        return n1 + n2;
     }
 
     // Método para realizar la resta de dos números complejos
     public String resta(String n1, String n2) {
-        throw new UnsupportedOperationException("No implementada");
+    return n1-n2;    
     }
 
     // Método para realizar la multiplicación de dos números complejos
     public String multiplicacion(String n1, String n2) {
-        throw new UnsupportedOperationException("No implementada");
+    return n1*n2;
     }
 
     // Método para realizar la división de dos números complejos
     public String division(String n1, String n2) {
-        throw new UnsupportedOperationException("No implementada");
+    return n1/n2;
     }
 }
 
